26f55599ab3cfbfe40dc7284db2f6379a8a9a435
Brian Peiris <brianpeiris@gmail.com>
2021-01-21 20:54:50 -0500

    Refactor. Add features for testing output devices

--- index.js
+++ index.js
@@ -2,120 +2,360 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class Root extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      status: "",
-      devices: [],
-      tracks: [],
-      constraints: [],
-      meter: 0
-    };
+class AudioDeviceManager {
+  constructor() {
+    this.currentInputDevice = null;
+    this.currentOutputDevice = null;
+
+    this.audioElement = document.createElement("audio");
+    this.audioElement.src = "guitar.mp3";
+    this.audioElement.loop = true;
+    this.audioElement.volume = 0.2;
+    document.body.append(this.audioElement);
+
+    this.createContext();
   }
-  componentDidMount() {
-    setInterval(() => {
-      if (!this.analyser) return;
-      this.analyser.getByteFrequencyData(this.data);
-      let sum = 0;
-      for (let i = 0; i < this.data.length; i++) {
-        sum += this.data[i] / 255;
-      }
-      sum = sum / this.data.length;
-      this.setState({ meter: sum });
-    }, 5);
-    document.addEventListener("click", () => {
-      if (!this.ctx || this.ctx.state !== "suspended") return;
+
+  createContext() {
+    if (this.ctx) {
+      this.ctx.close();
+    }
+    if (this.audioBuffer) {
+      this.audioBuffer.stop();
+    }
+
+    this.ctx = new (window.AudioContext || window.webkitAudioContext)();
+
+    this.outputNode = this.ctx.createGain();
+    this.outputNode.connect(this.ctx.destination);
+
+    this.analyser = this.ctx.createAnalyser();
+    this.analyser.connect(this.outputNode);
+    this.analyser.fftSize = 32;
+    this.analyserData = new Uint8Array(this.analyser.frequencyBinCount);
+
+    this.audioBuffer = null;
+    this.decodedAudioData = null;
+
+    if (this.currentInputDevice) {
+      this.setInputDevice(this.currentInputDevice);
+    }
+    if (this.currentOutputDevice) {
+      this.setOutputDevice(this.currentOutputDevice);
+    }
+  }
+
+  resumeContext() {
+    if (this.ctx.state === "suspended") {
       this.ctx.resume();
-    });
+    }
   }
-  getConstraints() {
-    this.setState({ constraints: navigator.mediaDevices.getSupportedConstraints() });
+
+  async getDevices(kind) {
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+
+    const devices = (await navigator.mediaDevices.enumerateDevices()).filter((device) => device.kind === kind);
+    devices.sort((a, b) => (a.label.toLowerCase() > b.label.toLowerCase() ? 1 : -1));
+
+    // We need to call getUserMedia media in order to enumerateDevices with labels.
+    // Then we have to stop the audio track, since Firefox thinks we're still using
+    // the stream if we try to switch devices later.
+    for (const track of stream.getAudioTracks()) {
+      track.stop();
+    }
+
+    return devices;
   }
-  async getMedia() {
-    this.setState({ status: "getting media" });
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({audio: true});
-      const devices = (await navigator.mediaDevices.enumerateDevices()).filter(d => d.kind === "audioinput");
-      this.setState({ devices });
-      this.setState({ status: "got media" });
-      // We need to call getUserMedia media in order to enumerateDevices with labels.
-      // Then we have to stop the audio track, since Firefox thinks we're still using
-      // the stream if we try to switch devices later.
-      for (const track of stream.getAudioTracks()) {
+
+  async setInputDevice(deviceId) {
+    this.currentInputDevice = deviceId;
+
+    if (this.currentSource) {
+      this.currentSource.disconnect();
+    }
+
+    if (this.inputStream) {
+      for (const track of this.getInputTracks()) {
         track.stop();
       }
+    }
+
+    this.inputStream = await navigator.mediaDevices.getUserMedia({ audio: { deviceId } });
+    this.currentSource = this.ctx.createMediaStreamSource(this.inputStream);
+    this.currentSource.connect(this.analyser);
+  }
+
+  async setOutputDevice(deviceId) {
+    this.currentOutputDevice = deviceId;
+
+    this.outputNode.disconnect();
+    const dest = this.ctx.createMediaStreamDestination();
+    this.outputNode.connect(dest);
+
+    const audioOutput = new Audio();
+    audioOutput.srcObject = dest.stream;
+    audioOutput.setSinkId(deviceId);
+    audioOutput.play();
+
+    this.audioElement.setSinkId(deviceId);
+  }
+
+  getAnalyserLevel() {
+    this.analyser.getByteFrequencyData(this.analyserData);
+    let sum = 0;
+    for (let i = 0; i < this.analyserData.length; i++) {
+      sum += this.analyserData[i] / 255;
+    }
+    sum = sum / this.analyserData.length;
+    return sum;
+  }
+
+  getInputTracks() {
+    return this.inputStream.getAudioTracks();
+  }
+
+  getConstraints() {
+    return navigator.mediaDevices.getSupportedConstraints();
+  }
+
+  toggleAudioElement() {
+    if (this.audioElement.paused) {
+      this.audioElement.play();
+    } else {
+      this.audioElement.pause();
+    }
+  }
+
+  async toggleAudioBuffer() {
+    if (this.audioBuffer) {
+      this.audioBuffer.stop();
+      this.audioBuffer = null;
+
+      return true;
+    } else {
+      this.audioBuffer = this.ctx.createBufferSource();
+
+      if (!this.decodedAudioData) {
+        const audioData = await fetch("beat.mp3").then((resp) => resp.arrayBuffer());
+        this.decodedAudioData = await this.ctx.decodeAudioData(audioData);
+      }
+
+      this.audioBuffer.buffer = this.decodedAudioData;
+
+      this.audioBuffer.connect(this.outputNode);
+      this.audioBuffer.loop = true;
+      this.audioBuffer.start();
+
+      return false;
+    }
+  }
+}
+
+class AudioMeter extends React.Component {
+  state = {
+    value: 0
+  }
+  updateValue = () => {
+    const value = this.props.audioDeviceManager.getAnalyserLevel();
+    this.setState({ value });
+    window.requestAnimationFrame(this.updateValue);
+  }
+  componentDidMount() {
+    requestAnimationFrame(this.updateValue);
+  }
+  render() {
+    return (
+      <div id="meter">
+        <div id="bar" style={{ width: `${this.state.value * 100}%` }} />
+      </div>
+    );
+  }
+}
+
+class Devices extends React.PureComponent {
+  render() {
+    const { title, value: devices, setDevice } = this.props;
+
+    return (
+      <>
+        <h2>{title}:</h2>
+        {devices.length === 0 ? "- no devices -" : null}
+        {devices.map((device, i) => (
+          <div key={i} className="device">
+            <button onClick={() => setDevice(device.deviceId)}>use</button>
+            <div key={i} className="deviceInfo">
+              <label>label:</label>
+              {device.label}
+              <br />
+
+              <label>kind:</label>
+              <span style={{ backgroundColor: stringToColor(device.kind) }}>{device.kind}</span>
+              <br />
+
+              <label>deviceId:</label>
+              <span style={{ backgroundColor: stringToColor(device.deviceId) }}>{device.deviceId}</span>
+              <br />
+
+              <label>groupId:</label>
+              <span style={{ backgroundColor: stringToColor(device.groupId) }}>{device.groupId}</span>
+              <br />
+            </div>
+          </div>
+        ))}
+      </>
+    );
+  }
+}
+
+function stringToColor(str) {
+  var hash = 0;
+  for (var i = 0; i < str.length; i++) {
+    hash = (hash + str.charCodeAt(i) * 20) % 255;
+  }
+  return `hsl(${(hash / 255) * 360}, 100%, 90%)`;
+}
+
+class Root extends React.PureComponent {
+  state = {
+    status: "",
+    inputDevices: [],
+    outputDevices: [],
+    inputTracks: [],
+    constraints: [],
+    meter: 0,
+    audioElementPaused: true,
+    audioBufferPaused: true,
+  };
+
+  audioDeviceManager = new AudioDeviceManager();
+
+  componentDidMount() {
+    document.addEventListener("click", () => this.audioDeviceManager.resumeContext());
+  }
+
+  getConstraints() {
+    this.setState({ constraints: this.audioDeviceManager.getConstraints() });
+  }
+
+  async getDevices(kind, stateProp) {
+    try {
+      this.setState({ status: "getting devices" });
+
+      const devices = await this.audioDeviceManager.getDevices(kind);
+
+      this.setState({ [stateProp]: devices });
+      this.setState({ status: devices.length ? "got devices" : "no devices found" });
     } catch (e) {
-      this.setState({ status: `error getting media ${e}` });
+      this.setState({ status: `error getting devices ${e}` });
       console.error(e);
     }
   }
-  async stopAnalyser() {
-    if (this.currentSource) this.currentSource.disconnect(this.analyser);
-    if (this.ctx) await this.ctx.close();
-    this.analyser = null;
-    this.data = null;
-    this.ctx = null;
-  }
-  startAnalyser(stream) {
-    this.ctx = new (window.AudioContext || window.webkitAudioContext)();
-    this.analyser = this.ctx.createAnalyser();
-    this.analyser.fftSize = 32;
-    this.data = new Uint8Array(this.analyser.frequencyBinCount);
-    this.currentSource = this.ctx.createMediaStreamSource(stream);
-    this.currentSource.connect(this.analyser);
-    this.analyser.connect(this.ctx.destination);
-  }
-  async useDevice(deviceId) {
+
+  async setInputDevice(deviceId) {
     this.setState({ status: "getting device" });
-    for (const track of this.state.tracks) {
-      track.stop();
-    }
-    await this.stopAnalyser();
-    this.setState({ tracks: [] });
+    this.setState({ inputTracks: [] });
     try {
-      const stream = await navigator.mediaDevices.getUserMedia({
-        audio: { deviceId: deviceId }
-      });
+      await this.audioDeviceManager.setInputDevice(deviceId);
       this.setState({ status: "got device" });
-      this.setState({ tracks: stream.getAudioTracks() });
-      this.startAnalyser(stream);
+      this.setState({ inputTracks: await this.audioDeviceManager.getInputTracks() });
     } catch (e) {
       this.setState({ status: `error getting device ${e}` });
       console.error(e);
     }
   }
+
+  async setOutputDevice(deviceId) {
+    this.setState({ status: "setting output device" });
+    this.audioDeviceManager.setOutputDevice(deviceId);
+    this.setState({ status: "set output device" });
+  }
+
+  toggleAudioElement() {
+    this.audioDeviceManager.toggleAudioElement();
+    this.setState((state) => ({audioElementPaused: !state.audioElementPaused}));
+  }
+
+  async toggleAudioBuffer() {
+    const audioBufferPaused = await this.audioDeviceManager.toggleAudioBuffer();
+    this.setState({ audioBufferPaused });
+  }
+
+  recreateContext() {
+    this.setState({ status: "recreating context" });
+    this.audioDeviceManager.createContext();
+    this.setState({ audioBufferPaused: true });
+    this.setState({ status: "context recreated" });
+  }
+
   render() {
-    const track = this.state.track;
     return (
       <>
         <h1>mic test</h1>
-        <div id="status">{this.state.status}</div>
-        <div id="meter">
-          <div id="bar" style={{ width: `${this.state.meter * 100}%` }} />
-        </div>
-        {this.state.tracks.map((track, i) => (
+
+        <div id="status">status: {this.state.status || "-"}</div>
+
+        <AudioMeter audioDeviceManager={this.audioDeviceManager} />
+
+        <button onClick={() => this.getDevices("audioinput", "inputDevices")}>
+          get input devices
+        </button>
+
+        <button onClick={() => this.getDevices("audiooutput", "outputDevices")}>
+          get output devices
+        </button>
+
+        <button onClick={() => this.toggleAudioElement()}>
+          {this.state.audioElementPaused ? "play" : "pause"} audio element
+        </button>
+
+        <button onClick={() => this.toggleAudioBuffer()}>
+          {this.state.audioBufferPaused ? "play" : "pause"} audio buffer
+        </button>
+
+        <button onClick={() => this.recreateContext()}>
+          recreate context
+        </button>
+
+        <button onClick={() => this.getConstraints()}>
+          get constraints
+        </button>
+
+        <h2>input tracks:</h2>
+        {this.state.inputTracks.length === 0 ? "- no input tracks -" : null}
+        {this.state.inputTracks.map((track, i) => (
           <div key={i} className="track">
-            <label>label:</label> {track.label} <br />
-            <label>enabled:</label> {"" + track.enabled} <br />
-            <label>muted:</label> {"" + track.muted} <br />
-            <label>readyState:</label> {track.readyState} <br />
+            <label>label:</label>
+            {track.label} <br />
+
+            <label>enabled:</label>
+            {String(track.enabled)} <br />
+
+            <label>muted:</label>
+            {String(track.muted)} <br />
+
+            <label>readyState:</label>
+            {track.readyState} <br />
           </div>
         ))}
-        <button onClick={() => this.getMedia()}>get media</button>
-        <button onClick={() => this.getConstraints()}>get constraints</button>
-        {this.state.devices.map((device, i) => (
-          <div key={i} className="device">
-            <button onClick={() => this.useDevice(device.deviceId)}>use</button>
-            <div key={i} className="deviceInfo">
-              <label>label:</label> {device.label} <br />
-            </div>
-          </div>
-        ))}
+
+        <Devices
+          title="input devices"
+          value={this.state.inputDevices}
+          setDevice={(deviceId) => this.setInputDevice(deviceId)}
+        />
+
+        <Devices
+          title="output devices"
+          value={this.state.outputDevices}
+          setDevice={(deviceId) => this.setOutputDevice(deviceId)}
+        />
+
+        <h2>constraints:</h2>
+        {Object.entries(this.state.constraints).length === 0 ? "- no constraints -" : null}
         {Object.entries(this.state.constraints).map(([name], i) => (
           <div key={i} className="constraint">
             <div key={i} className="constraintInfo">
-              <label>name:</label> {name} <br />
+              {name}
             </div>
           </div>
         ))}
@@ -127,4 +367,5 @@ class Root extends React.Component {
 const root = document.createElement("div");
 root.id = "root";
 document.body.append(root);
+
 ReactDOM.render(<Root />, root);
